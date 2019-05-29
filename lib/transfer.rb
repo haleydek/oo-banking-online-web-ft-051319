c15a45335810b5f16731a6bf09685b3fa2000989
Haley DeKeyser <hcd09@live.com>
2019-05-29 17:36:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount
   attr_reader :status
+  transaction_counter = 0
   
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -14,19 +15,21 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid?
-      @receiver.deposit(@amount)
-      @sender.balance -= @amount
-      if self.valid? == true
-        @status = "complete"
+    transaction_counter += 1
+    until transaction_counter > 1
+      if @sender.valid?
+        @receiver.deposit(@amount)
+        @sender.balance -= @amount
+        if self.valid? == true
+          @status = "complete"
+        else
+          @status = "rejected"
+          return "Transaction rejected. Please check your account balance."
+        end
       else
         @status = "rejected"
         return "Transaction rejected. Please check your account balance."
       end
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
   end
   
   def reverse_transfer
